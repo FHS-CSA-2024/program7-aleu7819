04bbd096ebbf53e23cdc02f69e84883589926c3a
aleu7819 <allison.asl.leung@gmail.com>
2024-08-28 12:09:32 -0700

    Update Program.java
    added scanners and prompts (old conversions)

--- Program.java
+++ Program.java
@@ -1,6 +1,43 @@
 //import stuff here
 
 //Your code here
+import java.util.Scanner;
+
+public class Program5 {
+    public static void main (String[] args) {
+        
+         Scanner numScanner = new Scanner(System.in);
+         
+         System.out.println("how many shrutebucks?");
+         int Shrutebucks = numScanner.nextInt();
+         
+          System.out.println("how many klevens?");
+        int Klevens = numScanner.nextInt();
+        
+        int StanNickels = Klevins/12
+        
+         System.out.println("how many stanley nickels?");
+         int StanNickels = numScanner.nextInt();
+         
+         double oldCon = Shrutebucks + Klevens + StanNickels;
+         System.out.println("Old monetary system: " + Shrutebucks + "." + Klevens + "." + StanNickels);
+         
+         //set old variables
+// set old conversion systems
+//x oldstannickel * 100newstannickel/240oldstannickel
+
+//kleven/12 = stan nickel
+//shrutebuck = stannickel/100
+
+
+// set new conversion systems
+// set new variables
+
+ 
+        
+}}
+
+
 
 //Paste console output below:
 /*
